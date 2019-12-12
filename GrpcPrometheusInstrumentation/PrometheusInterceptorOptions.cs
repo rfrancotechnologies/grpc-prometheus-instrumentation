4f5624d4649d7264f6de635db715b172ad8fb4b8
jsolana_cr <jsolana@rfranco.com>
2019-12-12 12:45:46 +0100

    Customize metrics.

--- GrpcPrometheusInstrumentation/PrometheusInterceptorOptions.cs
+++ GrpcPrometheusInstrumentation/PrometheusInterceptorOptions.cs
@@ -2,7 +2,7 @@ namespace Com.Rfranco.Instrumentation.Prometheus
 {
     public class PrometheusInterceptorOptions
     {
-        private static readonly double[] DefaultBuckets = {.1, .25, 1, 2.5, 5 };
+        private static readonly double[] DefaultBuckets = { .005, .01, .025, .05, .075, .1, .25, .5, .75, 1, 2.5, 5, 7.5, 10 };
 
         public double[] Buckets {get; set;} = DefaultBuckets;
     }
